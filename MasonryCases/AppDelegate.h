5cec8b1d5104529664da7d9b85b6efbcd8aec845
jianmei <shijianmei0@sina.com>
2018-07-17 10:43:40 +0800

    Initial Commit

---
+++ MasonryCases/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryCases
+//
+//  Created by Jianmei on 2018/7/17.
+//  Copyright © 2018 Jianmei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
